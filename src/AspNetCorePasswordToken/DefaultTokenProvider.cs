3cb9170d1f8810b7da076527d2f1d59c914053b9
kalpeshsatasiya <satasiya.kalpesh2006@gmail.com>
2016-06-26 23:37:57 +0530

    Initial commit

---
+++ src/AspNetCorePasswordToken/DefaultTokenProvider.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.DataProtection;
+using Microsoft.AspNet.Identity;
+using Microsoft.Extensions.OptionsModel;
+
+namespace AspNetCorePasswordToken
+{
+
+    public class DefaultDataProtectorTokenProvider<TUser> : DataProtectorTokenProvider<TUser> where TUser : class
+    {
+        public DefaultDataProtectorTokenProvider(IDataProtectionProvider dataProtectionProvider, IOptions<DefaultDataProtectorTokenProviderOptions> options) : base(dataProtectionProvider, options)
+        {
+        }
+    }
+
+    public class DefaultDataProtectorTokenProviderOptions : DataProtectionTokenProviderOptions { }
+
+}
